5da74654cfdd888b0d2d2cc4c07b569dc6496783
T0xen <jl.graham5002@gmail.com>
2025-10-24 04:52:55 -0400

    Created a new Java project that has no issues and wrote out addition, subtraction, and input validation

---
+++ Calculator/calc v2/src/Calc.java
@@ -0,0 +1,100 @@
+// Needed to read console input through BufferedReader
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Calc {
+
+    // run the calc functions here
+    public static void main(String[] args) throws Exception {
+        
+        //boolean userDone = false;
+        boolean isNumber = false;
+        float calculation = 0;
+        float a = 0, b = 0;
+        //while(userDone == false)
+        //{
+            BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+
+            // could extend to > 2 vars by asking number of digits and using a for loop + array
+            System.out.print("Enter a number (decimals are allowed): ");
+
+            // while loop to force user to enter a number until they do
+            // if it didn't kill the program when parsing the float if it's the wrong type this could be
+            // done through if statements, not these nested while-loop try catch blocks 
+            while(isNumber == false) {
+                try {
+                    a = Float.parseFloat(r.readLine());
+
+                    // forces that the user enters a number within the bounds while still checking for non-number inputs
+                    while(!Float.isFinite(a)) {
+                        System.out.print("Please enter a number that is within min/max bounds this time: ");
+
+                        try {
+                            a = Float.parseFloat(r.readLine());
+                        } catch (NumberFormatException e) {
+                            System.out.print("Error: Invalid input. ");
+                        }                        
+                    }
+
+                    isNumber = true;
+                } catch (NumberFormatException e) {
+                    System.out.print("Error: Invalid input; please enter a number this time: ");
+                }
+            }
+        
+            // reset so the next try-catch block repeats while necessary
+            isNumber = false;
+
+            System.out.print("Enter a number (decimals are allowed): ");
+            while(isNumber == false) {
+                try {
+                    b = Float.parseFloat(r.readLine());
+
+                    while(!Float.isFinite(b)) {
+                        System.out.print("Please enter a number that is within min/max bounds this time: ");
+
+                        try {
+                            b = Float.parseFloat(r.readLine());
+                        } catch (NumberFormatException e) {
+                            System.out.print("Error: Invalid input. ");
+                        }                        
+                    }
+
+                    isNumber = true;
+                } catch (NumberFormatException e) {
+                    System.out.print("Error: Invalid input; please enter a number this time: ");
+                }
+            }
+
+            System.out.print("What operations would you like to perform (Add/Subtract): ");
+            String operation = r.readLine();
+
+            // used if statement at first but switches exist lol
+            switch(operation.toLowerCase()) {
+                case "add":
+                    calculation = add(a, b);
+                    break;
+                case "+":
+                    calculation = add(a, b);
+                    break;
+                case "subtract":
+                    calculation = subtract(a, b);
+                    break;
+                case "-":
+                    calculation = subtract(a, b);
+            }
+        
+            System.out.println(calculation);
+        //}
+    }
+    // add two variables
+    public static float add(float x, float y) {
+        return x + y;
+    }
+
+    // subtract two variables
+    public static float subtract(float x, float y) {
+        return x - y;
+    }
+
+}
