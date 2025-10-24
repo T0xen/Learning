2e9dd3922ac4841960e263f11a2dd0b2b6b95c6d
T0xen <jl.graham5002@gmail.com>
2025-10-24 18:31:18 -0400

    The program now runs for as long as the user does not wish to exit and now prints a report of what operations were performed and their outputs once the user exits the program

--- Calculator/calc v2/src/Calc.java
+++ Calculator/calc v2/src/Calc.java
@@ -1,25 +1,29 @@
 // Needed to read console input through BufferedReader
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList; // Need to do more than 2 var operations and store outputs
 
 public class Calc {
 
     // run the calc functions here
     public static void main(String[] args) throws Exception {
-        //boolean userDone = false;
+        boolean userDone = false;
         boolean isNumber = false;
         boolean validOp = false;
         float calculation = 0;
         float a = 0, b = 0;
-        //while(userDone == false)
-        //{
+        ArrayList<Float> calcs = new ArrayList<Float>();
+        ArrayList<String> operations = new ArrayList<String>();
+        int counter = 0;
+        while(userDone == false)
+        {
             BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
 
             // could extend to > 2 vars by asking number of digits and using a for loop + array
             System.out.print("Enter a number (decimals are allowed): ");
 
-            // while loop to force user to enter a number until they do
-            // if it didn't kill the program when parsing the float if it's the wrong type this could be
+            // while loop to force user to enter a number until they do didn't kill the 
+            // program when parsing the float if it's the wrong type this could be
             // done through if statements, not these nested while-loop try catch blocks 
             while(isNumber == false) {
                 try {
@@ -50,7 +54,6 @@ public class Calc {
             while(isNumber == false) {
                 try {
                     b = Float.parseFloat(r.readLine());
-
                     while(!Float.isFinite(b)) {
                         System.out.print("Please enter a number that is within min/max bounds this time: ");
 
@@ -78,47 +81,76 @@ public class Calc {
                 switch(operation.toLowerCase()) {
                     case "add":
                         calculation = add(a, b);
+                        operations.add("Addition");
                         validOp = true;
                         break;
                     case "+":
                         calculation = add(a, b);
+                        operations.add("Addition");
                         validOp = true;
                         break;
                     case "subtract":
                         calculation = subtract(a, b);
+                        operations.add("Subtraction");
                         validOp = true;
                         break;
                     case "-":
                         calculation = subtract(a, b);
+                        operations.add("Subtraction");
                         validOp = true;
                         break;
                     case "multiply":
                         calculation = multiply(a, b);
+                        operations.add("Multiplication");
                         validOp = true;
                         break;
                     case "*":
                         calculation = multiply(a, b);
+                        operations.add("Multiplication");
                         validOp = true;
                         break;
                     case "divide":
                         calculation = divide(a, b);
+                        operations.add("Division");
                         validOp = true;
                         break;
                     case "/":
                         calculation = divide(a, b);
+                        operations.add("Division");
                         validOp = true;
                         break;
                     default:
-                        validOp = false;
                         System.out.print("Please enter a valid operation this time (Add/Subtract/Multiply/Divide): ");
                         operation = r.readLine();
                         break;
                 } 
             }
             
+            // resets for the next iteration of the while program
+            isNumber = false;
+            validOp = false;
+
             System.out.println(calculation);
-        //}
+            calcs.add(calculation);
+            counter++;
+
+            System.out.print("Would you like to perform more operations (y/n): ");
+            String completed = r.readLine();
+            
+            if(completed.equalsIgnoreCase("no") | completed.equalsIgnoreCase("n")) {
+                userDone = true;
+            }
+        }
+
+        // calc output report (could extend to maybe show the math but eh, might include the operation performed)
+        int n = 1;
+        System.out.println("Number of operations performed: " + counter);
+        for (Float calc : calcs) {
+            System.out.printf("Operation %d (%s) output: %f \n", n, operations.get(n - 1), calc);
+            n++;
+        }
     }
+
     // add two variables
     public static float add(float x, float y) {
         return x + y;
